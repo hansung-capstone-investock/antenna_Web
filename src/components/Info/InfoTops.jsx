52bb3d04e79c3cf3bdde303d614a4da7b290ae3d
seakye <kiworld96@gmail>
2021-06-17 14:40:33 +0900

    build

--- src/components/Info/InfoTops.jsx
+++ src/components/Info/InfoTops.jsx
@@ -2,11 +2,24 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import { Tabs } from "antd";
+import { MText } from "../../components";
+import { Table, Tag, Space } from "antd";
+
+const { TabPane } = Tabs;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 const InfoTops = () => {
+  const [Data, setData] = useState();
   const [Date, setDate] = useState();
   const [Price, setPrice] = useState();
+  const [PriceTabel, setPriceTable] = useState();
   const [Cap, setCap] = useState();
+  const [CapTable, setCapTable] = useState();
 
   async function GetInfoTops(code) {
     const response = await axios.get(
@@ -17,13 +30,58 @@ const InfoTops = () => {
       setDate(response.data.date);
       setPrice(response.data.price);
       setCap(response.data.cap);
+      setData(response.data);
     }
   }
   useEffect(() => {
     GetInfoTops();
   }, []);
 
-  return <div>infoTops</div>;
+  const table = (data) => {
+    const arr = [];
+    data.map((top, idx) => {});
+
+    const col = [];
+    return (
+      <Table
+        className="table-striped-rows"
+        dataSource={arr}
+        columns={arr}
+        scroll={{ x: 0, y: 500 }}
+        pagination={false}
+      />
+    );
+  };
+  useEffect(() => {
+    table(Price);
+  }, [Data]);
+
+  const OnChange = (props) => {
+    console.log("onchange", props);
+  };
+
+  return (
+    <Wrapper>
+      <MText font="a아시아헤드2" size="20px" color="red">
+        Top
+        <MText color="black" padding="3px 0 0 0">
+          &nbsp;종목
+        </MText>
+      </MText>
+      <Tabs
+        onChange={OnChange}
+        type="card"
+        animated={{ inkBar: true, tabPane: false }}
+      >
+        <TabPane tab="상한가" key="1">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="거래량 상위" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+      </Tabs>
+    </Wrapper>
+  );
 };
 
 export default InfoTops;
